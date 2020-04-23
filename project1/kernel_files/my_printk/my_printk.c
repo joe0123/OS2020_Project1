1c770fd18f99776e627ed00643f1fe0ecdf1ea85
boewoei <boewoei0123@gmail.com>
2020-04-24 02:42:14 +0800

    kernel files

--- project1/kernel_files/my_printk/my_printk.c
+++ project1/kernel_files/my_printk/my_printk.c
@@ -1,9 +1,7 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 
-asmlinkage int
-sys_my_printk(const char* message)
-{
+asmlinkage int sys_my_printk(const char* message){
     printk(message);
     return 0;
 }
